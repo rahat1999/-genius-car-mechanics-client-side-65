14dfbb10ea1aab9acd33417283291cc51b0578c1
rahat1999 <rahat.is.1999@gmail.com>
2021-10-29 02:44:01 +0600

    Connect to the Databae Live url

--- src/Pages/Booking/Booking/Booking.js
+++ src/Pages/Booking/Booking/Booking.js
@@ -5,7 +5,7 @@ const Booking = () => {
     const { serviceId } = useParams();
     const [service, setService] = useState({})
     useEffect(() => {
-        const url = `http://localhost:5000/services/${serviceId}`
+        const url = `https://damp-mountain-14618.herokuapp.com/services/${serviceId}`
         fetch(url)
             .then(res => res.json())
             .then(data => setService(data))
--- src/Pages/ManageServices/ManageServices.js
+++ src/Pages/ManageServices/ManageServices.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 const ManageServices = () => {
     const [services, setServices] = useState([])
     useEffect(() => {
-        fetch('http://localhost:5000/services')
+        fetch('https://damp-mountain-14618.herokuapp.com/services')
             .then(res => res.json())
             .then(data => setServices(data))
     }, []);
@@ -13,7 +13,7 @@ const ManageServices = () => {
 
         const confirm = window.confirm('Are You sure You want to delete ?')
         if (confirm) {
-            const url = `http://localhost:5000/services/${id}`
+            const url = `https://damp-mountain-14618.herokuapp.com/services/${id}`
             fetch(url, {
                 method: "DELETE"
             })
